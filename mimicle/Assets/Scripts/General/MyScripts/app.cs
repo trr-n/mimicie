b75211a662f0806878f9d1d019afa82e7259ffed
cet <cets021@gmail.com>
2023-06-13 07:04:14 +0900

    style

--- mimicle/Assets/Scripts/General/MyScripts/app.cs
+++ mimicle/Assets/Scripts/General/MyScripts/app.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using static UnityEngine.Application;
 
 namespace Mimical.Extend
 {
+    public enum FPS { Low = 24, Medium = 30, High = 60, VSync = -1 }
+
     public static class app
     {
         public static void SetFps(int fps = -1)
-        => Application.targetFrameRate = fps;
+        => targetFrameRate = fps;
+
+        public static void SetFps(FPS fps)
+        => targetFrameRate = ((int)fps);
     }
 }
