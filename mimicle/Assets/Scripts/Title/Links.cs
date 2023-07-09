17e9eab90d6c2621c52ac56a39ee6ae4622d3e0c
cet <cets021@gmail.com>
2023-07-09 20:53:26 +0900

    a

---
+++ mimicle/Assets/Scripts/Title/Links.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mimical
+{
+    public class Links : MonoBehaviour
+    {
+        public void Exit() => Application.Quit();
+    }
+}
