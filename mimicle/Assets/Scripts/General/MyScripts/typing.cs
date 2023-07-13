b8cb59b4373c40fe616bebc8f767bd4eeef501f4
cet <67916285+cet-t@users.noreply.github.com>
2023-07-14 06:49:11 +0900

    t

--- mimicle/Assets/Scripts/General/MyScripts/typing.cs
+++ mimicle/Assets/Scripts/General/MyScripts/typing.cs
@@ -3,8 +3,5 @@ namespace UnionEngine.Extend
     public static class Typing
     {
         public static T Cast<T>(this object obj) => (T)(object)obj;
-        public static float Single(this object obj) => (float)obj;
-        public static int Int(this object obj) => (int)obj;
-        public static double Double(this object obj) => (double)obj;
     }
 }
